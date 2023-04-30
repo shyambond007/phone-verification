f3bba57feff0f50754893c47171cecfbdca3dfc8
shyam mohan pandey <shyammohan367@gmail.com>
2023-04-30 21:05:01 +0530

    Update server.js

--- PhoneVerification-main/server.js
+++ PhoneVerification-main/server.js
@@ -14,21 +14,21 @@ app.use(express.json())
 
 
 //API routes
-app.use("/api",require('./routes'))
+app.use("/api", require('./routes'))
 
 //Database connection here
-const USER_NAME = "admin"
-const PASSWORD = "admin"
+const USER_NAME = "shyammohan367"
+const PASSWORD = "vEycpHfDJFAls8QS"
 const DATABASE_NAME = "mobile_verify_db"
 
 //here connect function is used to connect the database cluster using connection string
-mongoose.connect(`mongodb+srv://${USER_NAME}:${PASSWORD}@cluster0.oq0vx.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`).then(()=>{
+mongoose.connect(`mongodb+srv://${USER_NAME}:${PASSWORD}@cluster0.fjgmitx.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`).then(() => {
     console.log("Database is connected successfully...!")
-}).catch((errorMessage)=>{
-    console.log("Error: Unable to connect database",errorMessage)
+}).catch((errorMessage) => {
+    console.log("Error: Unable to connect database", errorMessage)
 })
 
 //To start the main server
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
